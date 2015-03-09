f439575c1f35fe215fb42299cd335227961af668
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-03-09 14:48:52 +0530

    socio

---
+++ managesocio/Global.asax.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Web.Mvc
+Imports System.Web.Optimization
+Imports System.IdentityModel.Services
+
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Private Sub Application_Start(sender As Object, e As EventArgs)
+        ' Code that runs on application startup
+        AreaRegistration.RegisterAllAreas()
+        RouteConfig.RegisterRoutes(RouteTable.Routes)
+        BundleConfig.RegisterBundles(BundleTable.Bundles)
+        IdentityConfig.ConfigureIdentity()
+    End Sub
+
+
+    Sub WSFederationAuthenticationModule_RedirectingToIdentityProvider(sender As Object, e As RedirectingToIdentityProviderEventArgs)
+        If Not [String].IsNullOrEmpty(Realm) Then
+            e.SignInRequestMessage.Realm = Realm
+        End If
+    End Sub
+End Class
---
+++ managesocio/Site.Master.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.IdentityModel.Services
+Imports System.IdentityModel.Services.Configuration
+Imports System.Security.Claims
+Imports System.Security.Principal
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+
+Partial Public Class SiteMaster
+    Inherits MasterPage
+    Protected Sub Page_Load(sender As Object, e As EventArgs)
+
+    End Sub
+    Protected Sub Unnamed_LoggingOut(sender As Object, e As LoginCancelEventArgs)
+        Dim config As WsFederationConfiguration = FederatedAuthentication.FederationConfiguration.WsFederationConfiguration
+        ' Redirect to ~/Account/SignOut after signing out.
+        Dim callbackUrl As String = Request.Url.GetLeftPart(UriPartial.Authority) + Response.ApplyAppPathModifier("~/Account/SignOut")
+        Dim signoutMessage As New SignOutRequestMessage(New Uri(config.Issuer), callbackUrl)
+        signoutMessage.SetParameter("wtrealm", If(IdentityConfig.Realm, config.Realm))
+        FederatedAuthentication.SessionAuthenticationModule.SignOut()
+        Response.Redirect(signoutMessage.WriteQueryString())
+    End Sub
+End Class
