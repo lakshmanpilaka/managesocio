648cec054cd576b3c84d4446ae5d608c6882a593
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-03-11 23:59:15 +0530

    comm

--- managesocio/Messages.aspx
+++ managesocio/Messages.aspx
@@ -1,5 +1,29 @@
-﻿<%@ Page Title="Send Offers" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Messages.aspx.vb" Inherits="managesocio.Messages" %>
+﻿<%@ Page Title="Offer Management" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Messages.aspx.vb" Inherits="managesocio.Messages" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-  
-             
+       <%--<div class="dropdown ">
+        <button class="btn btn btn-default dropdown-toggle" type="button" id="empGroup" runat="server" data-toggle="dropdown">Select Employee Group
+        <span class="caret"></span></button>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="#">HTML</a></li>
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CSS</a></li>
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="#">JavaScript</a></li>
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="#">About Us</a></li>
+        </ul>
+      </div>--%>
+
+       <asp:SqlDataSource ID="socio_azure" runat="server" ConnectionString="<%$ ConnectionStrings:sociodata %>" SelectCommand="SELECT [CityId], [City] FROM [City]"></asp:SqlDataSource>
+      
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+    <div class="col-md-offset-3">
+        <asp:DropDownList AutoPostBack="true" ID="empGroup1" runat="server" CssClass="col-lg-10 selectpicker form-control" OnSelectedIndexChanged="empGroup1_SelectedIndexChanged">
+            <asp:ListItem Value="-1" Text="Please Select" Selected="True" />
+        </asp:DropDownList>
+    </div>
+
 </asp:Content>
+
+
--- managesocio/Messages.aspx.vb
+++ managesocio/Messages.aspx.vb
@@ -1,8 +1,55 @@
-﻿Public Class Messages
+﻿Imports System.Data.SqlClient
+
+Public Class Messages
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim ddEmpGroup As New DropDownList
+        If Not IsPostBack Then
+
+            Dim connStr As String
+            Dim selectStatement As String
+            Dim sqlAdapter As SqlDataAdapter
+            Dim dt As DataTable
+            Dim con As SqlConnection
+
+            connStr = "Data Source=mqjkakgxkk.database.windows.net,1433;Initial Catalog=sociodata;Persist Security Info=True;User ID=laks;Password=sqlPassword@123"
+
+            con = New SqlConnection(connStr)
+            selectStatement = "Select CityId,City from City"
+            sqlAdapter = New SqlDataAdapter(selectStatement, con)
+            dt = New DataTable()
+            sqlAdapter.Fill(dt)
+            empGroup1.DataSource = dt
+            empGroup1.DataBind()
+            empGroup1.DataTextField = "City"
+            empGroup1.DataValueField = "CityId"
+            empGroup1.DataBind()
+            empGroup1.Items.Insert(0, New ListItem("Please select employee group", "-1"))
+        End If
+
 
     End Sub
 
+
+
+    Protected Sub AddCity()
+        Dim MyConnection As SqlConnection
+        MyConnection = New SqlConnection("Data Source=mqjkakgxkk.database.windows.net,1433;Initial Catalog=sociodata;Persist Security Info=True;User ID=laks;Password=sqlPassword@123")
+        Dim insertCmd As String
+        insertCmd = "insert into CIty (City) values (' ');"
+        Dim myCommand As SqlCommand
+        myCommand = New SqlCommand(insertCmd, MyConnection)
+        myCommand.Connection.Open()
+        myCommand.ExecuteNonQuery()
+
+        'GridView1.DataBind()
+        'UpdatePanel1.UpdateMode = UpdatePanelUpdateMode.Conditional
+        'UpdatePanel1.Update()
+    End Sub
+
+    
+    Protected Sub empGroup1_SelectedIndexChanged(sender As Object, e As EventArgs)
+        MsgBox(empGroup1.SelectedItem.Value)
+    End Sub
 End Class
